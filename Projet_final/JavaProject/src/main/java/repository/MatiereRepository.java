bd786b78acc620465c72982058aaa6fa8c81192a
cyrilSeb <cyril@sebaux.com>
2018-05-01 23:15:49 +0200

    Test

---
+++ Projet_final/JavaProject/src/main/java/repository/MatiereRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.Matiere;
+
+public interface MatiereRepository extends JpaRepository<Matiere,Long>{
+
+}
---
+++ Projet_final/JavaProject/src/main/java/repository/SalleRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.Salle;
+
+public interface SalleRepository extends JpaRepository<Salle,Long>{
+
+}
