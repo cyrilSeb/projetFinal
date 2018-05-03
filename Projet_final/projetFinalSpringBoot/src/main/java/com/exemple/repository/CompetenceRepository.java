ec800b933aab05b7f136569d0e085c909516f39f
mirijason <mirijason@free.fr>
2018-05-03 18:05:04 +0200

    Create DONE for les User (attention, il reste à voir la partie Authentification)

--- Projet_final/projetFinalSpringBoot/src/main/java/com/exemple/repository/CompetenceRepository.java
+++ Projet_final/projetFinalSpringBoot/src/main/java/com/exemple/repository/CompetenceRepository.java
@@ -3,7 +3,7 @@ package com.exemple.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.exemple.model.Competence;
+import com.exemple.model.FormateurMatierePK;
 
-public interface CompetenceRepository extends JpaRepository<Competence, Long> {
-
+public interface CompetenceRepository extends JpaRepository<Competence, FormateurMatierePK> {
 }
