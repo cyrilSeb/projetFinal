20b96a802996b513e1ff1658a2a30bc133f42044
mirijason <mirijason@free.fr>
2018-05-03 11:18:50 +0200

    restController/CompetenceRestController.java

--- Projet_final/projetFinalSpringBoot/src/main/java/com/exemple/restController/CompetenceRestController.java
+++ Projet_final/projetFinalSpringBoot/src/main/java/com/exemple/restController/CompetenceRestController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +14,6 @@ import com.exemple.repository.CompetenceRepository;
 
 @RestController
 @RequestMapping("/rest/competence")
-@CrossOrigin(origins = { "*" })
 public class CompetenceRestController {
 
 	@Autowired
